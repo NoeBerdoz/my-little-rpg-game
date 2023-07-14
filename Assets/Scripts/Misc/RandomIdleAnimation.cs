9efac6c9930d73a79c6fa6de3c1312080507f024
NoeBerdoz <berdoznoe@gmail.com>
2023-07-14 18:15:57 +0200

    Add torch random animation

---
+++ Assets/Scripts/Misc/RandomIdleAnimation.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomIdleAnimation : MonoBehaviour
+{
+    private Animator myAnimator;
+
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        AnimatorStateInfo state = myAnimator.GetCurrentAnimatorStateInfo(0);
+        myAnimator.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
+    }
+}
