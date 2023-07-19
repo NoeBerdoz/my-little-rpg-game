9e1069916c6a3b39302f4490b829d220952a9abb
NoeBerdoz <berdoznoe@gmail.com>
2023-07-19 21:42:54 +0200

    IWeapon interface with refactored Sword

--- Assets/Scripts/Inventory/Sword.cs
+++ Assets/Scripts/Inventory/Sword.cs
@@ -2,18 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sword : MonoBehaviour
+public class Sword : MonoBehaviour, IWeapon
 {
     [SerializeField] private GameObject slashAnimPrefab;
     [SerializeField] private Transform slashAnimSpawnPoint;
     [SerializeField] private Transform weaponCollider;
     [SerializeField] private float swordAttackCD = .5f;
 
-    private PlayerControls playerControls;
     private Animator myAnimator;
     private PlayerController playerController;
     private ActiveWeapon activeWeapon;
-    private bool attackButtonDown, isAttacking = false;
 
     private GameObject slashAnim;
 
@@ -21,49 +19,26 @@ public class Sword : MonoBehaviour
         playerController = GetComponentInParent<PlayerController>();
         activeWeapon = GetComponentInParent<ActiveWeapon>();
         myAnimator = GetComponent<Animator>();
-        playerControls = new PlayerControls();
-    }
-
-    private void OnEnable() {
-        playerControls.Enable();
-    }
-
-    void Start()
-    {
-        // Call Attack function on control input Attack
-        playerControls.Combat.Attack.started += _ => StartAttacking(); // '_' synthax to not pass any parameters to the coming function
-        playerControls.Combat.Attack.canceled += _ => StopAttacking();
     }
 
     private void Update() {
         MouseFollowWithOffset();
-        Attack();
     }
 
-    private void StartAttacking() {
-        attackButtonDown = true;
-    }
+    public void Attack() {
+        // isAttacking = true;
+        myAnimator.SetTrigger("Attack");
+        weaponCollider.gameObject.SetActive(true);
 
-    private void StopAttacking() {
-        attackButtonDown = false;
-    }
-
-    private void Attack() {
-        if (attackButtonDown && !isAttacking) {
-            isAttacking = true;
-            myAnimator.SetTrigger("Attack");
-            weaponCollider.gameObject.SetActive(true);
-
-            slashAnim = Instantiate(slashAnimPrefab, slashAnimSpawnPoint.position, Quaternion.identity); // Quaternion to gives the same rotation as the prefab
-            // Set parent as active weapon in hierarchy
-            slashAnim.transform.parent = this.transform.parent;
-            StartCoroutine(AttackCDRoutine());
-        }
+        slashAnim = Instantiate(slashAnimPrefab, slashAnimSpawnPoint.position, Quaternion.identity); // Quaternion to gives the same rotation as the prefab
+        // Set parent as active weapon in hierarchy
+        slashAnim.transform.parent = this.transform.parent;
+        StartCoroutine(AttackCDRoutine());
     }
 
     private IEnumerator AttackCDRoutine() {
         yield return new WaitForSeconds(swordAttackCD);
-        isAttacking = false;
+        ActiveWeapon.Instance.ToggleIsAttacking(false);
     }
 
     public void DoneAttackingAnimEvent() {
--- Assets/Scripts/Player/ActiveWeapon.cs
+++ Assets/Scripts/Player/ActiveWeapon.cs
@@ -1,18 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ActiveWeapon : MonoBehaviour
+public class ActiveWeapon : Singleton<ActiveWeapon>
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private MonoBehaviour currentActiveWeapon;
+    
+    private PlayerControls playerControls;
+    private bool attackButtonDown, isAttacking = false;
+
+
+    protected override void Awake()
     {
+        base.Awake();
+        
+        playerControls = new PlayerControls();
+    }
+    
+    private void OnEnable() {
+        playerControls.Enable();
+    }
+
+    private void Start()
+    {
+        // Call Attack function on control input Attack
+        playerControls.Combat.Attack.started += _ => StartAttacking(); // '_' synthax to not pass any parameters to the coming function
+        playerControls.Combat.Attack.canceled += _ => StopAttacking();
+    }
+
+    private void Update()
+    {
+        Attack();
+    }
+
+    public void ToggleIsAttacking(bool value)
+    {
+        isAttacking = value;
+    }
+
+    private void StartAttacking() {
+        attackButtonDown = true;
+    }
+
+    private void StopAttacking() {
+        attackButtonDown = false;
+    }
+
+    private void Attack()
+    {
+        if (attackButtonDown && !isAttacking)
+        {
+            isAttacking = true;
+        
+            // Call the Attack() function from the correct class through the IWeapon interface
+            (currentActiveWeapon as IWeapon).Attack();
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
