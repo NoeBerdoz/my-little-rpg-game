7b41790f40b73cf4b540cea598a177031929a5e3
NoeBerdoz <berdoznoe@gmail.com>
2023-07-22 15:30:07 +0200

    Arrow animation

--- Assets/Scripts/Inventory/Bow.cs
+++ Assets/Scripts/Inventory/Bow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,24 @@ using UnityEngine;
 public class Bow : MonoBehaviour, IWeapon
 {
     [SerializeField] private WeaponInfo weaponInfo;
+    [SerializeField] private GameObject arrowPrefab;
+    [SerializeField] private Transform arrowSpwanPoint;
+
+    private Animator myAnimator;
+
+    // Peroformance optimisation
+    private readonly int FIRE_HASH = Animator.StringToHash("Fire");
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
 
     public void Attack()
     {
-        Debug.Log("Attacking with Bow");
+        // Trigger parameter "Fire" is set on Idle animation parameters
+        myAnimator.SetTrigger(FIRE_HASH);
+        GameObject newArrow =
+            Instantiate(arrowPrefab, arrowSpwanPoint.position, ActiveWeapon.Instance.transform.rotation);
     }
     public WeaponInfo GetWeaponInfo()
     {
